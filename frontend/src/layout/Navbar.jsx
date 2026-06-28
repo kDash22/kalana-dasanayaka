6e4525dc091833ab6bea89a55ba1709a393a4d79
Kalana Dasanayaka <kalana2295@gmail.com>
2026-06-28 11:43:10 +0530

    feat: add Hero section, custom AnimatedBorderButton, Navbar, and Tailwind CSS configuration with global styles

--- frontend/src/layout/Navbar.jsx
+++ frontend/src/layout/Navbar.jsx
@@ -1,5 +1,8 @@
+import { useState } from "react";
 import { Button } from "../components/Button";
 import { Menu } from "lucide-react";
+import { X } from "lucide-react";
+
 
 const navLinks = [
   { href: "#about", label: "About" },
@@ -9,8 +12,9 @@ const navLinks = [
 ];
 
 export const Navbar = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   return (
-    <header className="fixed top-0 left-0 right-0 bg-transparent py-5">
+    <header className="fixed top-0 left-0 right-0 bg-transparent py-5 z-50">
       <nav className="container mx-auto px-6 flex items-center justify-between">
         <a
           href="#"
@@ -40,25 +44,29 @@ export const Navbar = () => {
         </div>
 
         {/* Mobile Menu Button */}
-        <button className="md:hidden p-2 text-foreground">
-          <Menu size={24} />
+        <button className="md:hidden p-2 text-foreground" onClick={() => setIsMobileMenuOpen((prev) => !prev)}>
+          {isMobileMenuOpen ? <X size={24} /> : <Menu size={24} />}
         </button>
+      </nav>
 
-        {/*Mobile menu */}
-        <div className="md:hidden glass-strong">
-          <div>
+      {/*Mobile menu */}
+      {isMobileMenuOpen && (
+        <div className="md:hidden glass-strong animate-fade-in">
+          <div className="container mx-auto px-6 py-6 flex flex-col gap-4">
             {navLinks.map((link, index) => (
               <a
                 href={link.href}
                 key={index}
-                className="px-4 py-2 text-sm text-muted-foreground hover:text-foreground rounded-b-full hover:bg-surface"
+                className="text-lg text-muted-foreground hover:text-foreground py-2"
               >
                 {link.label}
               </a>
             ))}
+
+            <Button>Contact Me</Button>
           </div>
         </div>
-      </nav>
+      )}
     </header>
   );
 };
--- frontend/src/sections/Hero.jsx
+++ frontend/src/sections/Hero.jsx
@@ -1,7 +1,85 @@
+import { Button } from "@/components/Button";
+import { AnimatedBorderButton } from "@/components/AnimatedBorderButton";
+import { ArrowRight } from "lucide-react";
+import { Download } from "lucide-react";
+
 export const Hero = () => {
-    return(
-        <section>
-            <h1>Hero</h1>
-        </section>
-    )
-}
+  return (
+    <section className="relative min-h-screen flex items-center overflow-hidden">
+      {/* Bg */}
+      <div className="absolute inset-0">
+        <img
+          src="/hero-bg.jpg"
+          alt="Hero img"
+          className="w-full h-full object-cover opacity-40"
+        />
+      </div>
+      <div className="absolute inset-0 bg-gradient-to-b from-background/10 via-background/70 to-background"></div>
+
+      {/* Green Dots */}
+      <div className="absoulute inset-0 overflow-hidden pointer-events-none">
+        {[...Array(80)].map((_, i) => (
+          <div
+            className="absolute w-1.5 h-1.5 rounded-full opacity-60"
+            style={{
+              backgroundColor: "#20B2A6",
+              left: `${Math.random() * 100}%`,
+              top: `${Math.random() * 100}%`,
+              animation: `slow-drift ${15 + Math.random() * 20}s ease-in-out infinite`,
+              animationDelay: `${Math.random() * 5}s`,
+            }}
+          />
+        ))}
+      </div>
+
+      {/* Content */}
+      <div className="container mx-auto px-6 pt-32 pb-20 relative z-10">
+        <div className="grid lg:grid-cols-2 gap-12 items-center">
+          {/*Left Col - text */}
+          <div className="space-y-8">
+            <div className="">
+              <span className="inline-flex items-center gap-2 px-4 py2 rounded-full glass text-sm text-primary animate-fade-in-slow">
+                <span className="w-2 h-2 bg-primary rounded-full animate-pulse" />
+                Coding Enthusiast · Java & Python
+              </span>
+            </div>
+
+            {/* Headline */}
+            <div className="space-y-4">
+              <h1 className="text-5xl md:text-6xl lg:text-7xl font-bold leading-tight animate-fade-in-slow animation-delay-100">
+                Developing{" "}
+                <span className="text-primary glow-text">reliable</span>
+                <br /> soltuions with
+                <br />{" "}
+                <span className="font-mono italic font-bold text-white">
+                  precision
+                </span>{" "}
+              </h1>
+
+              <p className="text-lg text-muted-foreground max-w-lg animate-fade-in-slow animation-delay-300">
+                Hi, I'm Kalana Dasanayaka, an ICT undergraduate focused on Java
+                development and algorithmic problem solving. I build
+                logic-driven systems like game engines and rule-based bots, with
+                an emphasis on clean architecture. Currently expanding into
+                Python for automation and backend development.
+              </p>
+            </div>
+
+            {/* CTAs */}
+            <div className="flex items-center gap-4">
+              <Button size="lg">
+                Contact Me
+                <ArrowRight className="w-5 h-5" />
+              </Button>
+              <AnimatedBorderButton>
+                <Download className="w-5 h-5" />
+                Download CV
+              </AnimatedBorderButton>
+            </div>
+          </div>
+          {/*Right Col - dp */}
+        </div>
+      </div>
+    </section>
+  );
+};
