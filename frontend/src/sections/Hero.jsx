62022624b27628e6c80833ff610f33554ccd8487
Kalana Dasanayaka <kalana2295@gmail.com>
2026-06-30 06:48:22 +0530

    fixing the hero img alt rendering.

--- frontend/src/sections/Hero.jsx
+++ frontend/src/sections/Hero.jsx
@@ -26,7 +26,7 @@ export const Hero = () => {
       {/* Bg */}
       <div className="absolute inset-0">
         <img
-          src="/hero-bg.jpg"
+          src={`${import.meta.env.BASE_URL}hero-bg.jpg`}
           alt="Hero img"
           className="w-full h-full object-cover opacity-40"
         />
